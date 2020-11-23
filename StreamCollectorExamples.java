b988e5c0c38260a2d975c9055f624da147881664
Dinesh Dasaratha rao <ddinesh83@gmail.com>
2020-11-22 21:39:04 -0600

    file removed unneccessary lines

--- StreamCollectorExamples.java
+++ StreamCollectorExamples.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import com.dinesh.pojo.Student;
+
 
 public class StreamCollectorExamples {
 
@@ -21,24 +21,7 @@ public class StreamCollectorExamples {
 				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
 		System.out.println("Map::" + map);
-
-		List<Student> studentlist = new ArrayList<Student>();
-		// Adding Students
-		studentlist.add(new Student(11, "Jon", 22));
-		studentlist.add(new Student(22, "Steve", 18));
-		studentlist.add(new Student(33, "Lucy", 22));
-		studentlist.add(new Student(44, "Sansa", 23));
-		studentlist.add(new Student(55, "Maggie", 18));
-		// Fetching student names as List
-		List<String> studentNameList = studentlist.stream().
-				map(n -> n.getName()).collect(Collectors.toList());
 		
-		System.out.println(" Student Name List:"+studentNameList);
-		
-		//Getting the average Age 
-		
-		Double averageAge=studentlist.stream().collect(Collectors.averagingInt(stu -> stu.getAge()));
-		System.out.println(" The aveage age of the students: "+ averageAge);
 	}
 
 }
